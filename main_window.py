2698e808aa9d489df474b6b295f2d6e6aeb22a2e
Axel Díaz <diaz.axelio@gmail.com>
2015-03-10 13:33:40 -0430

    Añadiendo nuevas opciones al menu

--- main_window.py
+++ main_window.py
@@ -1,7 +1,7 @@
-from PyQt4.QtGui import (QWidget, QAction, QApplication)
+from PyQt4.QtGui import (QMainWindow, QAction, QApplication, QTextEdit, QStatusBar)
 
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -9,8 +9,19 @@ class MainWindow(QWidget):
         self.setWindowTitle(self.tr('MainWindow'))
         self.setMinimumSize(400, 300)
 
+        widget = QTextEdit()
+        self.setCentralWidget(widget)
+
         self.accionSalir = QAction('Salir', self)
         self.accionSalir.setShortcut('Ctrl+Q')
+        self.accionSalir.setStatusTip('Salir de la aplicacion')
+
+        # Barra de estado
+        self.setStatusBar(QStatusBar())
+
+        menu = self.menuBar()
+        menu_archivo = menu.addMenu('&Archivo')
+        menu_archivo.addAction(self.accionSalir)
 
 
 if __name__ == '__main__':
